c7bd230fbb9bcb8a9036b0ece3c9ff4c82e9074a
MichalGrochowski03 <michal.grochowski@onet.eu>
2025-11-13 19:29:50 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,9 +1,46 @@
-﻿
-namespace Simulator;
+﻿namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    private string _description = "Unknown";
+
+    public string Description
+    {
+        get => _description;
+        init => _description = ValidateDesc(value);
+    }
+
     public uint Size { get; set; } = 3;
+
+    public Animals()
+    {
+    }
+
+    public Animals(string description)
+    {
+        Description = description;
+    }
+    private string ValidateDesc(string inputDesc)
+    {
+        string processedDesc = (inputDesc ?? "").Trim();
+
+        if (processedDesc.Length > 15)
+        {
+            processedDesc = processedDesc.Substring(0, 15).TrimEnd();
+        }
+
+        if (processedDesc.Length < 3)
+        {
+            processedDesc = processedDesc.PadRight(3, '#');
+        }
+
+        if (processedDesc.Length > 0 && char.IsLower(processedDesc[0]))
+        {
+            processedDesc = char.ToUpper(processedDesc[0]) + processedDesc.Substring(1);
+        }
+
+        return processedDesc;
+    }
+
     public string Info => $"{Description} <{Size}>";
 }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -2,17 +2,68 @@
 
 public class Creature
 {
-    public string Name { get; set; }
-    public int Level { get; set; }
+    private string _name;
+    private int _level;
+    public string Name
+    {
+        get => _name;
+        init => _name = ValidateAndFormatName(value);
+    }
+
+    public int Level
+    {
+        get => _level;
+        init => _level = ValidateLevel(value);
+    }
+
+    public Creature()
+    {
+        Name = "Unknown";
+        Level = 1;
+    }
 
     public Creature(string name, int level = 1)
     {
         Name = name;
         Level = level;
-    }  
+    }
 
-    public Creature()
+    private string ValidateAndFormatName(string inputName)
     {
+        string processedName = (inputName ?? "").Trim(); // (inputName ?? "") obsługuje null
+
+        if (processedName.Length > 25)
+        {
+            processedName = processedName.Substring(0, 25).TrimEnd();
+        }
+
+        if (processedName.Length < 3)
+        {
+            processedName = processedName.PadRight(3, '#');
+        }
+
+        if (processedName.Length > 0 && char.IsLower(processedName[0]))
+        {
+            processedName = char.ToUpper(processedName[0]) + processedName.Substring(1);
+        }
+
+        return processedName;
+
+    }
+
+    private int ValidateLevel(int inputLevel)
+    {
+        if (inputLevel < 1) return 1;
+        if (inputLevel > 10) return 10;
+        return inputLevel;
+    }
+
+    public void Upgrade()
+    {
+        if (Level < 10)
+        {
+            _level += 1;
+        }
     }
     public void SayHi()
     {
